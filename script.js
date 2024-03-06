ee971fec8b5475afa4802eb10a6d687f03c3d284
KamyaJohar02 <kamyajohar21999@gmail.com>
2024-03-06 13:46:52 +0530

    Love letter

---
+++ script.js
@@ -0,0 +1,18 @@
+// click on the card to open and play the music.
+// music source: (free music) youtube audio library - Two Hearts (by TrackTribe).
+// tested in the Firefox browser.
+
+let input = document.getElementById("open");
+let audio = document.getElementById("player");
+
+input.addEventListener("click", function(){
+  if(audio.paused){
+    audio.play();
+    audio.currentTime = 0;
+    input.innerHTML = "Pause";
+  } else {
+    audio.pause();
+    input.innerHTML = "Play";
+  }
+});
+
